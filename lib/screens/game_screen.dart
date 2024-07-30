d584473e74dd5715ff057c586d5f4aca5fffb3c7
OmkarKhilari <omkarkhilari95@gmail.com>
2024-07-30 14:02:58 +0530

    some changes

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -3,13 +3,6 @@ import 'package:flutter/material.dart';
 class GameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Game Screen'),
-      ),
-      body: Center(
-        child: Text('Game Screen Placeholder'),
-      ),
-    );
+    return Placeholder();
   }
 }
